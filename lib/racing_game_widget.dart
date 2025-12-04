d29c52a3a16b25640a9029a22b08d2b3c8f13951
Naraka28 <eildan28@hotmail.com>
2025-12-04 15:07:11 -0700

    fixed scale for images and sound outside from app

--- lib/racing_game_widget.dart
+++ lib/racing_game_widget.dart
@@ -30,7 +30,7 @@ class RacingGameWidget extends StatefulWidget {
 }
 
 class _RacingGameWidgetState extends State<RacingGameWidget>
-    with SingleTickerProviderStateMixin {
+    with SingleTickerProviderStateMixin, WidgetsBindingObserver {
   late RacingGame _game;
   late AudioPlayer _gameAudioPlayer;
   bool _isPaused = false;
@@ -41,6 +41,7 @@ class _RacingGameWidgetState extends State<RacingGameWidget>
   @override
   void initState() {
     super.initState();
+    WidgetsBinding.instance.addObserver(this);
     _isVertical = widget.startVertical;
     _gameAudioPlayer = AudioPlayer();
     // Refrescar el HUD periódicamente para reflejar cambios del juego
@@ -148,10 +149,23 @@ class _RacingGameWidgetState extends State<RacingGameWidget>
 
   @override
   void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
     AudioManager.instance.clearCurrentPlayer(_gameAudioPlayer);
     _hudTicker.dispose();
     _gameAudioPlayer.dispose();
     super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+    // Pausar el juego automáticamente si la app se va a segundo plano
+    if (state == AppLifecycleState.paused ||
+        state == AppLifecycleState.inactive) {
+      if (!_isPaused && !_isGameOver) {
+        _togglePause();
+      }
+    }
   }
 
   bool get _shouldShowOrientationButton {
